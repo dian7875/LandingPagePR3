4c46a107564a909c326c9dfd8057543dc4f145e2
Dian7875 <dian7875.a@gmail.com>
2023-10-23 17:45:34 -0600

    FINAL FIX

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,31 @@
 window.addEventListener("load", function (event) {
   console.log("'Todos los recursos terminaron de cargar!");
-loadContened();
+  loadContened();
 });
 
-function loadContened(){
- loadFoot();
- loadImage();
- loadIntro();
- loadService();
+
+let RYCDATA;
+
+function fetchData() {
+  return fetch('./JS/mocks/RYC.json')
+    .then(response => response.json())
+    .then(RYC => {
+      RYCDATA = RYC;
+    });
 }
 
-function loadIntro(){
-  fetch('./JS/mocks/RYC.json')
-  .then(response => response.json())
-  .then(RYC => {
-    //Info General de RYC
-    this.document.getElementById("Dir").innerText = RYC.Direction;
-    this.document.getElementById("Tel").innerText = RYC.Tel;
-    this.document.getElementById("Mail").innerText = RYC.Correo;
-    const LogoRYC = document.getElementById("Logo");
-    this.document.getElementById("Description").innerHTML = `
+function loadIntro() {
+  const RYC = RYCDATA;
+  //Info General de RYC
+  this.document.getElementById("Dir").innerText = RYC.Direction;
+  this.document.getElementById("Tel").innerText = RYC.Tel;
+  this.document.getElementById("Mail").innerText = RYC.Correo;
+  const LogoRYC = document.getElementById("Logo");
+  this.document.getElementById("Description").innerHTML = `
     <p id="Conocenos" class="Des">${RYC.history}</p>
     `;
-    LogoRYC.src = RYC.Logo;
-    this.document.getElementById("Contac").innerHTML = `
+  LogoRYC.src = RYC.Logo;
+  this.document.getElementById("Contac").innerHTML = `
     <h3>En ${RYC.companyName} Nos preocupamos por tu opinion</h3>
     <br>
     <h3>Podes contactarnos de las siguientes maneras</h3>
@@ -38,64 +40,59 @@ function loadIntro(){
     <br>
     <h3>Tambien puedes envianos un mensaje con tus datos y te contaremos en cuando podamos</h3>
     `;
-  });
 }
 
-function loadService(){
-   //Servicios de RYC
-   fetch('./JS/mocks/RYC.json')
-  .then(response => response.json())
-  .then(RYC => {
-   const servContainer = document.getElementById("Serv");
-   RYC.servicios.forEach((servicio) => {
-     const ServiceCard = document.createElement("span");
-     ServiceCard.id = servicio.ServId;
-     ServiceCard.className = "ServCard";
+function loadService() {
+  //Servicios de RYC
+  const RYC = RYCDATA;
+  const servContainer = document.getElementById("Serv");
+  RYC.servicios.forEach((servicio) => {
+    const ServiceCard = document.createElement("span");
+    ServiceCard.id = servicio.ServId;
+    ServiceCard.className = "ServCard";
 
-     const serviceContainer = document.createElement("span");
-     serviceContainer.className = "ServiceContainer";
+    const serviceContainer = document.createElement("span");
+    serviceContainer.className = "ServiceContainer";
 
 
-     const servicePreview = document.createElement("span");
-     servicePreview.className = "ServicePreview";
-     if (parseInt(servicio.ServId) % 2 === 0) {
-     }
-     servicePreview.innerHTML = `
+    const servicePreview = document.createElement("span");
+    servicePreview.className = "ServicePreview";
+    if (parseInt(servicio.ServId) % 2 === 0) {
+    }
+    servicePreview.innerHTML = `
          <img src="${servicio.ico}">
          <h3>${servicio.Name}</h3>`;
 
-     const serviceInfo = document.createElement("span");
-     serviceInfo.className = "ServiceInfo";
-     serviceInfo.innerHTML = `
+    const serviceInfo = document.createElement("span");
+    serviceInfo.className = "ServiceInfo";
+    serviceInfo.innerHTML = `
              <p>${servicio.description}</p> `;
 
-     //Marcas relacionadas a los servicios
-     const marcasRelacionadas = RYC.Relacionados.filter((relacionado) => relacionado.id === servicio.ServId);
-     if (marcasRelacionadas.length > 0) {
-       const marca = document.createElement("span");
-       marca.className = "marcas";
-       marcasRelacionadas.forEach((relacionado) => {
-         const marcaImg = document.createElement("img");
-         marcaImg.src = relacionado.marca;
-         marca.appendChild(marcaImg);
-       });
-       serviceInfo.appendChild(marca);
-     }
+    //Marcas relacionadas a los servicios
+    const marcasRelacionadas = RYC.Relacionados.filter((relacionado) => relacionado.id === servicio.ServId);
+    if (marcasRelacionadas.length > 0) {
+      const marca = document.createElement("span");
+      marca.className = "marcas";
+      marcasRelacionadas.forEach((relacionado) => {
+        const marcaImg = document.createElement("img");
+        marcaImg.src = relacionado.marca;
+        marca.appendChild(marcaImg);
+      });
+      serviceInfo.appendChild(marca);
+    }
 
-     serviceContainer.appendChild(serviceInfo);
-     serviceContainer.appendChild(servicePreview);
+    serviceContainer.appendChild(serviceInfo);
+    serviceContainer.appendChild(servicePreview);
 
-     ServiceCard.appendChild(serviceContainer);
-     servContainer.appendChild(ServiceCard);
+    ServiceCard.appendChild(serviceContainer);
+    servContainer.appendChild(ServiceCard);
 
-   });
   });
+
 };
-function loadFoot(){
-  fetch('./JS/mocks/RYC.json')
-  .then(response => response.json())
-  .then(RYC => {
-//Footer
+function loadFoot() {
+  const RYC = RYCDATA;
+  //Footer
   this.document.getElementById("firstColum").innerHTML = `
   <img class"FIcon" src="${RYC.Logo}"/>
   <h2 class="FName"> ${RYC.companyName}</h2>
@@ -110,15 +107,12 @@ function loadFoot(){
     target.appendChild(fService);
   }
   )
-});
 };
 
-function loadImage(){
-  fetch('./JS/mocks/RYC.json')
-  .then(response => response.json())
-  .then(RYC => {
-   //Carrusel de imagenes de proyectos
-   RYC.carrusel.forEach(carrusel => {
+function loadImage() {
+  const RYC = RYCDATA;
+  //Carrusel de imagenes de proyectos
+  RYC.carrusel.forEach(carrusel => {
     const imagenContainer = document.getElementById("imagenes");
     const imagen = document.createElement("img");
     imagen.src = carrusel.img;
@@ -130,7 +124,7 @@ function loadImage(){
   const ListImg = document.getElementById("imagenes");
   const anterior = document.getElementById("anterior");
   const siguiente = document.getElementById("siguiente");
-  const porcentajeImagen = 33.3; 
+  const porcentajeImagen = 33.3;
   const numImagenes = ListImg.children.length;
   let index = 0;
 
@@ -154,5 +148,16 @@ function loadImage(){
     const desplazamiento = -index * porcentajeImagen;
     ListImg.style.transform = `translateX(${desplazamiento}%)`;
   }
-});
 };
+
+
+
+function loadContened() {
+  fetchData()
+    .then(() => {
+      loadFoot();
+      loadImage();
+      loadIntro();
+      loadService();
+    });
+}
